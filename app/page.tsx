d0ec793a41dc7c4f9bd70d6e3423133044d7e323
junkcarlo16 <ramosjohncarlo16@gmail.com>
2026-04-28 15:16:12 +0800

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,64 +2,42 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="mx-auto min-h-[844px] w-full max-w-[390px]">
+      <nav className="sticky top-0 z-50 flex h-16 items-center justify-between bg-white px-4 font-serif uppercase text-blue-950">
+        <span className="tracking-[0.3em]">Elysia Booking</span>
+        <button className="tracking-[0.1em] flex items-center justify-center bg-blue-950 px-5 py-2 text-sm text-white">
+          RESERVE
+        </button>
+      </nav>
+      <div className="relative h-140 bg-green-700">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+          src="/images/hotel1.webp"
+          alt="Hotel Image"
+          fill
+          loading="eager"
+          className="object-cover brightness-30"
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+        
+        {/*transparent overlay*/} 
+        <div className="absolute inset-0 bg-blue-950/60"></div>
+
+        <section className="relative z-10 flex h-full flex-col items-center justify-center gap-4">
+          <span className="block text-lg text-center uppercase tracking-[0.3em] text-white">
+            A Sanctuary of <br />Stillness
+          </span>
+
+          <span className="block font-serif text-white tracking-[0.05em]">
+            Experience Ethereal Grandeur
+          </span>
+          
+          <button className="border-l-4 border-orange-400 h-12 w-60 bg-blue-950 text-sm text-white">
+            Book a Room
+          </button>
+          <button className="h-12 w-60 border border-white bg-white/5 text-sm text-white backdrop-blur">
+            Check Availability
+          </button>
+        </section>
+      </div>
     </div>
   );
 }
