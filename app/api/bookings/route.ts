0d3300061b63aeef537c599db9464343af0431a9
junkcarlo16 <ramosjohncarlo16@gmail.com>
2026-05-01 13:55:46 +0800

    prisma changes

--- app/api/bookings/route.ts
+++ app/api/bookings/route.ts
@@ -1,5 +1,5 @@
 import { PrismaPg } from "@prisma/adapter-pg"
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "../../generated/prisma/client"
 
 const adapter = new PrismaPg({ connectionString: process.env.DIRECT_URL })
 const prisma = new PrismaClient({ adapter })
